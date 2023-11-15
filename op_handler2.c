15618578c5b06f92660433e5706d432bbf3e7d51
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-11-15 20:29:13 +0300

    Fix for removing element on Top Of the Stack

--- op_handler2.c
+++ op_handler2.c
@@ -18,5 +18,6 @@ void add(stack_t **head, unsigned int line_num)
 		exit(EXIT_FAILURE);
 	}
 	current->next->n = current->n + current->next->n;
-	*head = (*head)->next;
+	*head = current->next;
+	free(current);
 }
